2722f73f6b9acacd96c18ef56df9a04367db9632
oleg-cpu <arshavaoleg70@gmail.com>
2023-12-14 15:39:55 +0100

    Developed Parallax effect

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -15,6 +15,7 @@ public class Road : MonoBehaviour
 
         float speed = GameManager.Instance.gameSpeed / transform.localScale.x;
         meshRenderer.material.mainTextureOffset += Vector2.right * speed * Time.deltaTime;
+
     }
 
 }
